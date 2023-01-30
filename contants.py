e7147e817184803d1def1d2a63668de45ce9e71f
FelipeWcosta <83873110+FelipeWcosta@users.noreply.github.com>
2023-01-30 16:45:35 -0300

    Update contants.py

--- contants.py
+++ contants.py
@@ -3,5 +3,8 @@ ten = Const(10)
 minus_two = C(-2)
 print(ten.shape())
 print(minus_two.shape())
+#specified explicitly
 print(C(0).shape())
 print(Const(360, unsigned(8)).value)
+print(Const(129, signed(8)).value)
+print(Const(1, unsigned(0)).value)
